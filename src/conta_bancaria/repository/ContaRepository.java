135c0dc6807bfcfb917e4b414c5ee83cc05c823f
luizavpg-bit <luizavpg@gmail.com>
2026-06-30 08:21:41 -0300

    Refactor ContaRepository interface methods
    Removed redundant comment and added method to list accounts by holder.

--- src/conta_bancaria/repository/ContaRepository.java
+++ src/conta_bancaria/repository/ContaRepository.java
@@ -3,18 +3,17 @@ package conta_bancaria.repository;
 import conta_bancaria.model.Conta;
 
 public interface ContaRepository {
-
-	// CRUD da Conta
+	
+	// CRUD
 	public void listarTodas();
 	public void cadastrar(Conta conta);
-   	public void procurarPorNumero(int numero);
+	public void procurarPorNumero(int numero);
 	public void atualizar(Conta conta);
 	public void deletar(int numero);
+	public void listarPorTitular(String titular);
 	
 	// Métodos Bancários
 	public void sacar(int numero, float valor);
 	public void depositar(int numero, float valor);
 	public void transferir(int numeroOrigem, int numeroDestino, float valor);
-	
 }
-
